19851c1b59b9ffeeac00cdfb0d58c1dfa769a86b
LAPTOP-30CLG7SB\PRASHANTH <prashanth11391@gmail.com>
2024-12-24 20:08:55 +0530

    jdbc initial setup

---
+++ src/main/java/com/gscrud/gscrudJDBC/GscrudJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.gscrud.gscrudJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GscrudJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GscrudJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gscrud/gscrudJDBC/GscrudJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gscrud.gscrudJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GscrudJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
